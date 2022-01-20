21522454b4b4a529d9e11b2be676ea9f0ae6ec4e
acousticintel <acousticintelligenceafrica@gmail.com>
2022-01-20 16:05:32 +0300

    build fix

--- pages/offers.js
+++ pages/offers.js
@@ -1,17 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
-import { useRecoilState } from "recoil";
-import { modalState } from '../atoms/modalAtom';
 import Modal from '../components/modal';
-import { db } from '../firebase';
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
 
 export default function Offers() {
   const router = useRouter();
   const { data: session, loading } = useSession();
-  const [open, setOpen] = useRecoilState(modalState);
 
   const [posts, setPosts] = useState([]);
 
