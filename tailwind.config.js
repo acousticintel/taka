a183cc070649bc2e618d360b5429aaef03d0940a
acousticintel <acousticintelligenceafrica@gmail.com>
2022-01-24 13:17:30 +0300

    font fix v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    fontFamily: {
-      'body': ['"Outfit"', 'san-serif'],
-    }
-  },
-  variants: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "body": ["Outfit", ...defaultTheme.fontFamily.sans],
+      },
+
+    },
   },
   plugins: [],
 }
