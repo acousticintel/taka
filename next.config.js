15b95b05b668524e93eb3d2233549a444014c828
acousticintel <acousticintelligenceafrica@gmail.com>
2022-02-21 18:41:30 +0300

    pwa v1.0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['http://www.potlabicons.com/','lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
   },
-}
+  pwa:{
+    dest:"public",
+    register:true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  }
+})
