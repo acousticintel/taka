18a74bbefdd14437bae2c325d8e613d7ad6e1a61
acousticintel <acousticintelligenceafrica@gmail.com>
2022-03-10 19:51:41 +0300

    banner test 1.1

--- components/layout/banner.js
+++ components/layout/banner.js
@@ -1,8 +1,24 @@
 import { useState, useEffect } from "react";
+//custom packs
+import { AnimatePresence } from "framer-motion";
+
+const bannerVar = {
+  hide: {
+    opacity: 0,
+    x: -15,
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.25,
+      ease: "easeIn",
+    },
+  },
+};
 
 export default function Banner() {
-
-  const [show, setShow ] = useState(false);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     // Initialize deferredPrompt for use later to show browser install prompt.
@@ -20,16 +36,24 @@ export default function Banner() {
     });
   });
 
-  const showInstallPromotion = () =>{
+  const showInstallPromotion = () => {
     setShow(true);
   };
 
   return (
-    <div className="banner">
-      <div className="content">
-        <span>Add the Taka application to your HomeScreen</span>
-        <button>Install</button>
-      </div>
-    </div>
+    <AnimatePresence exitBeforeEnter className="banner">
+      {show && (
+        <motion.div
+          variants={bannerVar}
+          initial="hide"
+          animate="show"
+          exit="hide"
+          className="content"
+        >
+          <span>Add the Taka application to your HomeScreen</span>
+          <button>Install</button>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 }
